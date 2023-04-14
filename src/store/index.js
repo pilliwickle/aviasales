a8619c2a773d6f680242157a8c48bde6f6783bad
pilliwickle <kurkovvalera@gmail.com>
2023-04-14 09:52:31 +0300

    change filters item

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+// eslint-disable-next-line import/no-named-as-default
 import filterSlice from './filterSlice'
 import searchIDSlice from './searchIDSlice'
 import ticketSlice from './ticketSlice'
